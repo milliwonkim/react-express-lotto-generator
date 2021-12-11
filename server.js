679039920658305cd042f5f82f7d5e5a2aedb3ed
milliwonkim <rldnjs9347@gmail.com>
2021-12-11 22:50:31 +0900

    [feat] add lotto generating logic

--- server.js
+++ server.js
@@ -2,26 +2,73 @@ const path = require('path');
 const express = require('express');
 const axios = require('axios');
 const app = express(); // create express app
+const cors = require('cors');
+const roundNumber = require('./src/lotto.json');
+
+app.use(cors());
 
 app.use(express.static(path.join(__dirname, 'public', 'index.html')));
 app.use(express.static('public'));
 
-const getHtml = async () => {
-    try {
+const getWholeWinningNumber = async () => {
+    // const lastEnd = roundNumber[0].roundNumber;
+    const lastEnd = 10;
+
+    const winningArray = [];
+
+    for (let i = 0; i < lastEnd; i++) {
         const response = await axios.get(
-            'https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo=861'
+            `https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo=${
+                i + 1
+            }`
         );
-        console.log(response.data);
-        return response;
+
+        const {
+            drwtNo1,
+            drwtNo2,
+            drwtNo3,
+            drwtNo4,
+            drwtNo5,
+            drwtNo6,
+            bnusNo,
+            drwNo,
+            drwNoDate,
+        } = response.data;
+
+        const winning = {
+            no1: drwtNo1,
+            no2: drwtNo2,
+            no3: drwtNo3,
+            no4: drwtNo4,
+            no5: drwtNo5,
+            no6: drwtNo6,
+            bonusNumber: bnusNo,
+            roundDate: drwNoDate,
+            roundNumber: drwNo,
+        };
+
+        winningArray.push(winning);
+    }
+
+    return winningArray;
+};
+
+app.get('/lotto', async (req, res) => {
+    try {
+        const winningNumber = await getWholeWinningNumber();
+        return res.send(winningNumber);
     } catch (error) {
         console.log('error: ', error);
     }
-};
+});
 
-getHtml();
-
-app.get('/', (req, res) => {
-    res.send({ DATA: getHtml().json() });
+app.post('/lotto', (req, res) => {
+    try {
+        console.log('req.body: ', req.body);
+        console.log('res.body: ', res.body);
+    } catch (error) {
+        console.log('post lotto error: ', error);
+    }
 });
 
 // start express server on port 5000
--- src/App.js
+++ src/App.js
@@ -1,97 +1,212 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import XLSX from 'xlsx';
+
 import './App.css';
 
-/* list of supported file types */
-const SheetJSFT = [
-    'xlsx',
-    'xlsb',
-    'xlsm',
-    'xls',
-    'xml',
-    'csv',
-    'txt',
-    'ods',
-    'fods',
-    'uos',
-    'sylk',
-    'dif',
-    'dbf',
-    'prn',
-    'qpw',
-    '123',
-    'wb*',
-    'wq*',
-    'html',
-    'htm',
-]
-    .map(function (x) {
-        return '.' + x;
-    })
-    .join(',');
-
 function App() {
-    const [data, setData] = useState({});
+    const [array, setArray] = useState([]);
+    const [isLoading, setIsLoading] = useState(0);
+
+    let i = 0;
+    const recursiveLimit = 2000;
+
+    const getCurrentNumber = (limitFor) => {
+        const digit = Math.floor(Math.random() * (limitFor + 1));
+
+        return digit;
+    };
+
+    const getRandomNumber = (previousNumber, limitFor) => {
+        const currentNumber = getCurrentNumber(limitFor);
+
+        if (previousNumber && previousNumber > 0 && currentNumber > 0) {
+            if (previousNumber < currentNumber) {
+                return currentNumber;
+            }
+
+            if (previousNumber >= currentNumber && i < recursiveLimit) {
+                i += 1;
+                return getRandomNumber(previousNumber, limitFor);
+            }
+        }
+    };
+
+    const injectLottoFreq = (freqObject, key, no) => {
+        if (no) {
+            if (freqObject[`${key}`][`${no}`]) {
+                freqObject[`${key}`][`${no}`] += 1;
+            } else {
+                freqObject[`${key}`][`${no}`] = 1;
+            }
+        }
+    };
+
+    const getSort = (object) => {
+        const sortByDesc = Object.entries(object).sort(([, a], [, b]) => b - a);
+        return Object.fromEntries(sortByDesc);
+    };
+
+    // TODO 1번째 당첨번호에서 무슨 번호가 가장 많이 등장했는지 순서별로 확인 필요
+    const getRankByColumn = (response) => {
+        const freqObject = {
+            no1: {},
+            no2: {},
+            no3: {},
+            no4: {},
+            no5: {},
+            no6: {},
+            bonusNumber: {},
+        };
+
+        response.data.forEach((element) => {
+            const { no1, no2, no3, no4, no5, no6, bonusNumber } = element;
+            injectLottoFreq(freqObject, 'no1', no1);
+            injectLottoFreq(freqObject, 'no2', no2);
+            injectLottoFreq(freqObject, 'no3', no3);
+            injectLottoFreq(freqObject, 'no4', no4);
+            injectLottoFreq(freqObject, 'no5', no5);
+            injectLottoFreq(freqObject, 'no6', no6);
+            injectLottoFreq(freqObject, 'bonusNumber', bonusNumber);
+        });
+
+        const { no1, no2, no3, no4, no5, no6, bonusNumber } = freqObject;
+
+        freqObject[no1] = getSort(no1);
+        freqObject[no2] = getSort(no2);
+        freqObject[no3] = getSort(no3);
+        freqObject[no4] = getSort(no4);
+        freqObject[no5] = getSort(no5);
+        freqObject[no6] = getSort(no6);
+        freqObject[bonusNumber] = getSort(bonusNumber);
+
+        return freqObject;
+    };
+
+    const getValidNumber = (number, response) => {
+        setIsLoading(3);
+
+        const newAlreadyWonData = response.map((el) => {
+            return {
+                no1: el.no1,
+                no2: el.no2,
+                no3: el.no3,
+                no4: el.no4,
+                no5: el.no5,
+                no6: el.no6,
+            };
+        });
+
+        return newAlreadyWonData.filter((el) => {
+            const StandardAlreadyWonData =
+                el.no1 === number.no1 &&
+                el.no2 === number.no2 &&
+                el.no3 === number.no3 &&
+                el.no4 === number.no4;
+
+            if (StandardAlreadyWonData) {
+                return returnTempLottoNumber(response);
+            }
+
+            setIsLoading(0);
+            setArray([...array, number]);
+
+            return number;
+        });
+    };
+
+    const returnTempLottoNumber = (responseData) => {
+        setIsLoading(2);
+
+        // limit
+        // 40 41 42 43 44 45
+        const limitFor1 = 40;
+        const limitFor2 = 41;
+        const limitFor3 = 42;
+        const limitFor4 = 43;
+        const limitFor5 = 44;
+        const limitFor6 = 45;
+
+        const randomNumber1 = Math.floor(Math.random() * (limitFor1 + 1));
+        const randomNumber2 =
+            randomNumber1 && getRandomNumber(randomNumber1, limitFor2);
+        const randomNumber3 =
+            randomNumber2 && getRandomNumber(randomNumber2, limitFor3);
+        const randomNumber4 =
+            randomNumber3 && getRandomNumber(randomNumber3, limitFor4);
+        const randomNumber5 =
+            randomNumber4 && getRandomNumber(randomNumber4, limitFor5);
+        const randomNumber6 =
+            randomNumber5 && getRandomNumber(randomNumber5, limitFor6);
+        const randomBonusNumber = Math.floor(Math.random() * (44 + 1));
+
+        const tempLottoNumber = {
+            no1: randomNumber1,
+            no2: randomNumber2,
+            no3: randomNumber3,
+            no4: randomNumber4,
+            no5: randomNumber5,
+            no6: randomNumber6,
+            bonusNumber: randomBonusNumber,
+        };
+
+        if (
+            randomNumber1 < randomNumber2 &&
+            randomNumber2 < randomNumber3 &&
+            randomNumber3 < randomNumber4 &&
+            randomNumber4 < randomNumber5 &&
+            randomNumber5 < randomNumber6 &&
+            randomNumber1 > 0 &&
+            randomBonusNumber > 0
+        ) {
+            return getValidNumber(tempLottoNumber, responseData);
+        } else {
+            return returnTempLottoNumber(responseData);
+        }
+    };
 
     const getLotto = async () => {
-        const response = await axios.get('http://localhost:5000');
-        console.log('response', response);
+        setIsLoading(1);
+        const response = await axios.get('http://localhost:5000/lotto');
+
+        console.log('getRankByColumn(response)', getRankByColumn(response));
+        getRankByColumn(response);
+        returnTempLottoNumber(response.data);
     };
 
     useEffect(() => {
         getLotto();
     }, []);
 
-    /* generate an array of column objects */
-    const make_cols = (refstr) => {
-        let o = [],
-            C = XLSX.utils.decode_range(refstr).e.c + 1;
-        for (var i = 0; i < C; ++i)
-            o[i] = { name: XLSX.utils.encode_col(i), key: i };
-        return o;
-    };
-
-    const handleChange = (e) => {
-        const files = e.target.files;
-        if (files && files[0]) handleFile(files[0]);
-    };
-
-    const handleFile = (file /*:File*/) => {
-        /* Boilerplate to set up FileReader */
-        const reader = new FileReader();
-        const rABS = !!reader.readAsBinaryString;
-        reader.onload = (e) => {
-            /* Parse data */
-            const bstr = e.target.result;
-            const wb = XLSX.read(bstr, { type: rABS ? 'binary' : 'array' });
-            /* Get first worksheet */
-            const wsname = wb.SheetNames[0];
-            const ws = wb.Sheets[wsname];
-            console.log(rABS, wb);
-            /* Convert array of arrays */
-            const data = XLSX.utils.sheet_to_json(ws, { header: 1 });
-            /* Update state */
-            setData({ data: data, cols: make_cols(ws['!ref']) });
-        };
-        if (rABS) reader.readAsBinaryString(file);
-        else reader.readAsArrayBuffer(file);
+    const renderLottoClient = () => {
+        switch (isLoading) {
+            case 0:
+                return array.map((el, i) => {
+                    const { no1, no2, no3, no4, no5, no6, bonusNumber } = el;
+                    return (
+                        <div key={i}>
+                            {`${no1} ${no2} ${no3} ${no4} ${no5} ${no6} * ${bonusNumber}`}{' '}
+                            <br />
+                        </div>
+                    );
+                });
+            case 1:
+                console.log('Get Lotto From Server');
+                return <p>Get Lotto From Server</p>;
+            case 2:
+                console.log('Generate Lotto Number');
+                return <p>Generate Lotto Number</p>;
+            default:
+                console.log('Get Valid Lotto Number(Verify)');
+                console.log('------------------------------');
+                return <p>Get Valid Lotto Number(Verify)</p>;
+        }
     };
 
     return (
         <div className='App'>
-            <form className='form-inline'>
-                <div className='form-group'>
-                    <label htmlFor='file'>Spreadsheet</label>
-                    <input
-                        type='file'
-                        className='form-control'
-                        id='file'
-                        accept={SheetJSFT}
-                        onChange={handleChange}
-                    />
-                </div>
-            </form>
+            <button onClick={getLotto}>생산</button>
+            <br />
+            {renderLottoClient()}
         </div>
     );
 }
